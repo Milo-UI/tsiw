2edede6bc2e5d05b729e58875e5f6cb592ace20a
milo-ui-web-design <17987794+Milo-UI@users.noreply.github.com>
2025-11-03 14:26:13 +0100

    lez

---
+++ esercizi/layout-flex-responsive/js/script.js
@@ -0,0 +1,11 @@
+let openBtn = document.querySelector('.open-btn');
+let closeBtn = document.querySelector('.close-btn');
+let menu = document.querySelector('nav ul');
+
+openBtn.addEventListener('click', () => {
+    menu.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    menu.classList.remove('open');
+})
